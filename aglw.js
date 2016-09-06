49e2c5aea690555f636051716f648c392efacc79
Аббас Гусенов <gusenov@live.ru>
2016-09-06 12:01:37 +0600

    Added OOP

--- aglw.js
+++ aglw.js
@@ -14,5 +14,17 @@ var AGLibWinJS = (function () {
         WinJS.Namespace.defineWithParent(parent, childName, childMembers);
     };
     
+    AGLibWinJS.createClass = function (constructor, instanceMembers, staticMembers) {
+        return WinJS.Class.define(constructor, instanceMembers, staticMembers);
+    };
+    
+    AGLibWinJS.deriveClass = function (baseClass, constructor, instanceMembers, staticMembers) {
+        return WinJS.Class.derive(baseClass, constructor, instanceMembers, staticMembers);
+    };
+    
+    AGLibWinJS.createMixin = function (constructor, mixin) {
+        return WinJS.Class.mix(constructor, mixin);
+    };
+    
     return AGLibWinJS;
 }());
